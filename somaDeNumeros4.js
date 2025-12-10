7337eaf2e7b183040cab34ac51821b2e89ca13e2
JuniorSoares-dev <olaviojunior67@gmail.com>
2025-12-09 21:03:55 -0300

    Limpa os campos de entrada ao clicar no botão

---
+++ somaDeNumeros4.js
@@ -0,0 +1,14 @@
+let input1 = document.getElementById("num1");
+let input2 = document.getElementById("num2");
+
+function botaoSomar(){
+    let valor1 = Number(input1.value);
+    let valor2 = Number(input2.value);
+
+    let soma = (valor1 + valor2);
+
+    let resultado = document.getElementById("resultado").textContent = soma;
+
+    input1.value = ''
+    input2.value = ''
+}
